edfcc343cae9beb09e25387c5223528869194b9d
dayanafabiola04@gmail.com <dayanafabiola04@gmail.com>
2025-12-20 21:49:43 +0000

    orders aggregation

---
+++ models/intermediate/int_orders_margin.sql
@@ -0,0 +1,20 @@
+with
+
+sales_margin as (
+    select *
+    from {{ ref('int_sales_margin') }}
+),
+
+aggregated as (
+    select
+        orders_id,
+        MAX(date_date) as date_date,
+        SUM(revenue) as revenue,
+        SUM(quantity) as quantity,
+        SUM(purchase_cost) as purchase_cost,
+        SUM(margin) as margin
+    from sales_margin
+    group by orders_id
+)
+
+select * from aggregated
