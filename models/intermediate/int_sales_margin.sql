3fd1ff9c0fd17833893ca92144d329ace1bb15d3
Dayana Bonifaz <dayanafabiola04@gmail.com>
2026-01-09 01:38:22 +0000

    macros

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,8 @@
         s.quantity,
         p.purchase_price,
         (s.quantity * p.purchase_price) as purchase_cost,
-        (s.revenue - (s.quantity * p.purchase_price)) as margin
+        (s.revenue - (s.quantity * p.purchase_price)) as margin,
+        {{ margin_percent('s.revenue', '(s.quantity * p.purchase_price)') }} AS margin_percent
     from {{ ref('stg_raw__sales') }} s
     left join {{ ref('stg_raw__product') }} p
         on s.products_id = p.products_id
