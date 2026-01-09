7e04df75270f2db5bb46af9e886926b66e94831a
Dayana Bonifaz <dayanafabiola04@gmail.com>
2026-01-09 00:27:57 +0000

    corrección en query de int_campaigns_day.sql

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -3,8 +3,8 @@ SELECT
   paid_source,
   COUNT(DISTINCT campaign_key) AS total_campaigns,
   SUM(ads_cost) AS total_ads_cost,
-  SUM(impressions) AS total_impressions,
-  SUM(clicks) AS total_clicks
+  SUM(impression) AS total_impressions,
+  SUM(click) AS total_clicks
 FROM {{ ref('int_campaigns') }}
 GROUP BY date_date, paid_source
 ORDER BY date_date DESC, paid_source
